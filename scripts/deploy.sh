0f9d7edefb90e34cb93348eeccfafcfd427f91d6
Serge Chastel <schastel@hawaii.edu>
2020-10-16 13:49:18 -1000

    Moved from hg to git. Deployment script uses jq

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
-VERSION=`cat /home/schastel/workspace/CFHTEphemerides/build.gradle | grep ^version | sed 's/.*= //g' | sed 's/"//g' | sed 's/\\s\\s//g'`
+VERSION=`jq -r .neo.project.version neo/project.json`
+
+rm -rf tmp && mkdir -p tmp
 
 echo $VERSION > VERSION
-hg log > tmp/CHANGELOG
+git log > tmp/CHANGELOG
 echo "" > tmp/README
 echo "java -jar CFHTEphemerides-$VERSION.jar -h" >> tmp/README
 echo "" >> tmp/README
 java -jar build/libs/CFHTEphemerides-$VERSION.jar -h >> tmp/README
 echo "" >> tmp/README
+
+if [ "$1" != "-doIt" ]; then
+  echo "Check tmp/CHANGELOG and tmp/README"
+  echo "Then execute $0 -doIt"
+  exit 0
+fi
 
 scp VERSION mops@nmops15:/var/www/html/users/cfht/VERSION
 scp tmp/CHANGELOG mops@nmops15:/var/www/html/users/cfht/CHANGELOG
