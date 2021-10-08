eb0cfc4f5fa1913e6bc3ffc6b96974b85a665c01
Serge Chastel <schastel@hawaii.edu>
2021-10-08 09:13:46 -1000

    Deprecated Time class replaced by Epoch

--- src/main/java/neo/cfht/models/Ephemeris.java
+++ src/main/java/neo/cfht/models/Ephemeris.java
@@ -11,13 +11,13 @@ import com.google.gson.JsonObject;
 import neo.models.astronomy.Declination;
 import neo.models.astronomy.RightAscension;
 import neo.models.base.Angle;
-import neo.models.time.Time;
+import neo.models.time.Epoch;
 
 public class Ephemeris {
 	/** Logging */
 	private final static Logger logger = LoggerFactory.getLogger(Ephemeris.class);
 
-	private Time epoch;
+	private Epoch epoch;
 	private RightAscension ra;
 	private Declination de;
 	private String mpcLine;
@@ -41,7 +41,7 @@ public class Ephemeris {
 		int hour = Integer.parseInt(mpcLine.substring(11, 11+2));
 		int minute = Integer.parseInt(mpcLine.substring(13, 13+2));
 		double dday = (double) day + ((double) hour)/24. + ((double) minute)/1440.;
-		ephemeris.epoch = Time.createFromMPCUTDate(year, month, dday);
+		ephemeris.epoch = Epoch.createFromMPCUTDate(year, month, dday);
 		// RA
 		ephemeris.ra = new RightAscension(Angle.fromRightAscensionHMS(mpcLine.substring(18, 24+4), " "));
 		// De
@@ -59,7 +59,7 @@ public class Ephemeris {
 		int minute = Integer.parseInt(time.substring(14, 14+2));
 		int second = Integer.parseInt(time.substring(17, 17+2));
 		double dday = (double) day + ((double) hour)/24. + ((double) minute)/1440. + ((double) second)/86400.;
-		ephemeris.epoch = Time.createFromMPCUTDate(year, month, dday);
+		ephemeris.epoch = Epoch.createFromMPCUTDate(year, month, dday);
 		ephemeris.ra = RightAscension.fromDegrees(raDeg);
 		ephemeris.de = Declination.fromDegrees(deDeg);
 		return ephemeris;
